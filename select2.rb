f6a4c419741b67e63a6e87511dd95079cab00d8a
Chris Nixon <nixoncd@mail.uc.edu>
2012-02-07 02:30:12 -0500

    I finished it...I need a beer! I hope it is actually fully working.

--- select2.rb
+++ select2.rb
@@ -5,62 +5,56 @@
 
 class Select2
   def select2(select_array, t, low, high)
+    remainder = 0
+    median_array = []
     puts "low: #{low} ---- high: #{high} ---- #{t}"
-    puts "SELECT ARRAY: #{select_array[low..high]}"
+    puts "SELECT ARRAY: #{select_array}"
     if high - low + 1 <= 5
       x = ad_hoc(select_array, t, low, high)
       puts "ADHOC: The value in the sorted array #{x[1]} at index #{t} is #{x[0]}."
       return x[0]
-    else
-      median_array = []
-      q = ((high - low + 1).to_f/5.to_f).floor
-      qtemp = q
-      a1, a2 = low, low+4
-      q.times do
-        median_array << median_of_five(select_array[a1..a2])
-        a1 += 5
-        a2 += 5
-      end
-      avg_median_value = nil
-      remainder = 0
-      if ((high - low + 1) % 5) != 0
-        remainder = (high - low +1) % 5
-        puts "REMAINDER: #{remainder}"
-        if remainder == 1
-          array_leftover = select_array.last
-          median_array << array_leftover
+    end
+    q = ((high - low + 1).to_f/5.to_f).floor
+    a1, a2 = low, low+4
+    q.times do
+      median_array << median_of_five(select_array[a1..a2])
+      a1 += 5
+      a2 += 5
+    end
+    if ((high - low + 1) % 5) != 0
+      remainder = (high - low +1) % 5
+      if remainder == 1
+        median_array << select_array.last
+      else
+        array_leftover = select_array[select_array.length-remainder..select_array.length-1].sort
+        if array_leftover.length % 2 == 0
+          median_array << array_leftover[array_leftover.length/2]
         else
-          array_leftover = select_array[select_array.length-remainder..select_array.length-1].sort
-          if array_leftover.length % 2 == 0
-            median_array << array_leftover[array_leftover.length/2]
-          else
-            avg_median_value = array_leftover[((array_leftover.length+1)/2)-1]
-            median_array << avg_median_value
-          end
+          median_array << array_leftover[((array_leftover.length-1)/2)]
         end
-        puts "LEFTOVER: #{array_leftover}"
       end
-      puts "MEDIAN ARRAY: #{median_array}"
-      pivot = select2(median_array, (qtemp-1)/2, 0, qtemp - 1)
-      puts "PIVOT: #{pivot}"
-      puts "INDEX OF PIVOT: #{select_array.index(pivot)}"
-      index_pivot = select_array.index(pivot)
-      temp = select_array[low]
-      select_array[low] = select_array[index_pivot]
-      select_array[index_pivot] = temp
-      puts "Modified select array: #{select_array}"
-      partition = partition2(select_array, low, high)
-      position = partition[0]
-      puts "Position #{position} and t: #{t}"
-      puts "Partition array: #{partition[1]}"
-      if t == position
-        x = select_array[position]
-        puts "We found it: #{x}"
-      elsif t < position
-        select2(select_array, t, low, position - 1)
-      elsif t > position
-        select2(select_array, t, position + 1, high)
-      end
+      q += 1 #necessary when we append the median from the overflow elements not in a group of 5
+    end
+    puts "MEDIAN ARRAY: #{median_array}"
+    pivot = select2(median_array, (q-1)/2, 0, q-1)
+    puts "PIVOT: #{pivot}"
+    puts "INDEX OF PIVOT: #{select_array.index(pivot)}"
+    index_pivot = select_array.index(pivot)
+    temp = select_array[low]
+    select_array[low] = select_array[index_pivot]
+    select_array[index_pivot] = temp
+    puts "Modified select array: #{select_array}"
+    partition = partition2(select_array, low, high)
+    position = partition[0]
+    puts "Position #{position} and t: #{t}"
+    puts "Partition array: #{partition[1]}"
+    if t == position
+      x = select_array[position]
+      puts "We found it: #{x}"
+    elsif t < position
+      select2(select_array, t, low, position - 1)
+    elsif t > position
+      select2(select_array, t, position + 1, high)
     end
   end
 
@@ -131,14 +125,10 @@ class Select2
   end
 
   def ad_hoc(select_array, t, low, high)
-    if high-low+1 == 5 && t <= 4
-      return select_array[low..high].sort[t], select_array[low..high].sort
+    if select_array[low..high].length % 2 != 0
+      return select_array[low..high].sort[t-low], select_array[low..high].sort
     else
-      if select_array[low..high].length % 2 != 0
-        return select_array[low..high].sort[t-low], select_array[low..high].sort
-      else
-        return select_array[low..high].sort[t-low], select_array[low..high].sort
-      end
+      return select_array[low..high].sort[t-low], select_array[low..high].sort
     end
   end
 
